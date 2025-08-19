6acebdc7d62375e6471d0ed818e28d472a3b4579
Molitor István <istvan.molitor@centralmediacsoport.hu>
2025-08-19 23:22:48 +0200

    init

---
+++ src/Hu24ArticleParser.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Services;
+
+use Molitor\HtmlParser\HtmlParser;
+
+class Hu24ArticleParser extends ArticleParser
+{
+    public function getPortal(): string
+    {
+        return '24.hu';
+    }
+
+    public function getSlug(): string
+    {
+        return $this->url->getPath();
+    }
+
+    public function isValidArticle(): bool
+    {
+        return true;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->html->getElementByFirstClassName('o-post__title')->stripTags();
+    }
+
+    public function getMainImageSrc(): string
+    {
+        return $this->html->getElementByFirstClassName('o-post__featuredImage')->getAttribute('src');
+    }
+
+    public function getMainImageAlt(): string
+    {
+        return $this->html->getElementByFirstClassName('o-post__featuredImage')->getAttribute('alt');
+    }
+
+    public function getCreatedAt(): string
+    {
+        return $this->html->getElementByFirstClassName('o-post__date')->stripTags();
+    }
+
+    public function getMainImageAuthor(): string
+    {
+        return '';
+    }
+
+    public function getLead(): string
+    {
+        $pTag = $this->html->getElementByFirstClassName('u-onlyArticlePages')->getElementByFirstTagName('p');
+        if($pTag) {
+            return $pTag->stripTags();
+        }
+        return '';
+    }
+
+    public function getBody(): array
+    {
+        $body = [];
+        $elements = $this->html->getElementByFirstClassName('u-onlyArticlePages')->getChildren();
+
+        /** @var HtmlParser $element */
+        foreach($elements as $element) {
+            $type = $element->getFirstTagName();
+            if($type === 'p') {
+                $body[] = [
+                    'type' => 'paragraph',
+                    'content' => $element->stripTags(),
+                ];
+            }
+            elseif($type === 'ul') {
+                $items = [];
+                foreach ($element->getElementsByTagName('li') as $li) {
+                    $items[] = $li->stripTags();
+                }
+                $body[] = [
+                    'type' => 'list',
+                    'items' => $items,
+                ];
+            }
+        }
+        return $body;
+    }
+
+    public function getAuthor(): string
+    {
+        return '';
+    }
+}
---
+++ src/IndexArticleParser.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Services;
+
+class IndexArticleParser extends ArticleParser
+{
+    public function getPortal(): string
+    {
+        return 'index.hu';
+    }
+
+    public function isValidArticle(): bool
+    {
+        return true;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->html->getElementByFirstClassName('content-title')->stripTags();
+    }
+
+    public function getMainImageSrc(): string
+    {
+        return $this->html->getElementByFirstClassName('cikk-cover')->getElementByFirstTagName('img')->getAttribute('src');
+    }
+
+    public function getMainImageAlt(): string
+    {
+        return $this->html->getElementByFirstClassName('cikk-cover')->getElementByFirstTagName('img')->getAttribute('alt');
+    }
+
+    public function getCreatedAt(): string
+    {
+        return '';
+    }
+
+    public function getMainImageAuthor(): string
+    {
+        return '';
+    }
+
+    public function getLead(): string
+    {
+        return $this->html->getElementByFirstClassName('lead_container')->stripTags();
+    }
+
+    public function getBody(): array
+    {
+        $body = [];
+        $elements = $this->html->getElementByFirstClassName('cikk-torzs')->getElementsByTagName('p');
+        foreach($elements as $element) {
+            $type = $element->getFirstTagName();
+            if($type === 'p') {
+                $body[] = [
+                    'type' => 'paragraph',
+                    'content' => $element->stripTags(),
+                ];
+            }
+            elseif($type == 'ul') {
+                $items = [];
+                foreach ($element->getElementsByTagName('li') as $li) {
+                    $items[] = $li->stripTags();
+                }
+                $body[] = [
+                    'type' => 'list',
+                    'items' => $items,
+                ];
+            }
+        }
+        return $body;
+    }
+
+    public function getAuthor(): string
+    {
+        return '';
+    }
+}
